f26185ef2f10d695835d1ab95c1f6d766415cd51
ADMIN <ADMIN@MSI>
2022-03-16 15:54:54 +0700

    Add project files.

---
+++ Tuan4_NguyenCongMinh/Controllers/GioHangController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan4_NguyenCongMinh.Models;
+
+namespace Tuan4_NguyenCongMinh.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        MyDataDataContext data = new MyDataDataContext();
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> listGiohang = Session["Giohang"] as List<Giohang>;
+            if (listGiohang == null)
+            {
+                listGiohang = new List<Giohang>();
+                Session["Giohang"] = listGiohang;
+            }
+            return listGiohang;
+        }
+
+        public ActionResult ThemGiohang(int id, string strURl)
+        {
+            List<Giohang> listGiohang = Laygiohang();
+
+            Giohang sanpham = listGiohang.Find(n => n.masach == id);
+
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(id);
+                listGiohang.Add(sanpham);
+                return Redirect(strURl);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURl);
+            }
+
+        }
+
+        private int TongSoLuong()
+        {
+            int tsl = 0;
+            List<Giohang> listGiohang = Session["GioHang"] as List<Giohang>;
+
+            if (listGiohang != null)
+            {
+                tsl = listGiohang.Sum(n => n.iSoluong);
+            }
+            return tsl;
+        }
+
+        private int TongSoLuongSanPham()
+        {
+            int tsl = 0;
+            List<Giohang> listGiohang = Session["GioHang"] as List<Giohang>;
+            if (listGiohang != null)
+            {
+                tsl = listGiohang.Count;
+            }
+            return tsl;
+        }
+
+        private double TongTien()
+        {
+            double tt = 0;
+            List<Giohang> listGiohang = Session["GioHang"] as List<Giohang>;
+            if (listGiohang != null)
+            {
+                tt = listGiohang.Sum(n => n.dThanhtien);
+            }
+            return tt;
+        }
+
+        public ActionResult GioHang()
+        {
+            List<Giohang> listGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.TongSoLuongSanPham = TongSoLuongSanPham();
+            return View(listGiohang);
+
+        }
+
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.TongSoLuongSanPham = TongSoLuongSanPham();
+            return PartialView();
+        }
+
+        public ActionResult XoaGiohang(int id)
+        {
+            List<Giohang> listGiohang = Laygiohang();
+
+            Giohang sanpham = listGiohang.SingleOrDefault(n => n.masach == id);
+            if (sanpham != null)
+            {
+                listGiohang.RemoveAll(n => n.masach == id);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapnhatGiohang(int id, FormCollection collection)
+        {
+            List<Giohang> listGiohang = Laygiohang();
+
+            Giohang sanpham = listGiohang.SingleOrDefault(n => n.masach == id);
+            if (sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(collection["txtSolg"].ToString());
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult XoatatcaGiohang()
+        {
+            List<Giohang> listGiohang = Laygiohang();
+            listGiohang.Clear();
+            return RedirectToAction("GioHang");
+        }
+       
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
